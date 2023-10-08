a3f34bae9abaf1a04ba08a4bdbce47f2fb542c9b
lone-ion <stilos@gmail.com>
2023-10-08 09:27:43 +0300

    finish exercise 1.2

--- part1-exercises/src/App.jsx
+++ part1-exercises/src/App.jsx
@@ -3,24 +3,37 @@ const Header = (props) => {
     <div>
       <h1>{props.course}</h1>
     </div>
-  )
-}
+  );
+};
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <div>
-      <p>{props.lesson} {props.exercise}</p>
+      <p>
+        {props.name} {props.hours}
+      </p>
     </div>
-  )
-}
+  );
+};
+
+const Content = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <Part name={props.lesson[0]} hours={props.exercise[0]} />
+      <Part name={props.lesson[1]} hours={props.exercise[1]} />
+      <Part name={props.lesson[2]} hours={props.exercise[2]} />
+    </div>
+  );
+};
 
 const Total = (props) => {
   return (
     <div>
       <p>Number of exercises: {props.hours}</p>
     </div>
-  )
-}
+  );
+};
 
 const App = () => {
   const course = "Half Stack application development";
@@ -34,12 +47,13 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-			<Content lesson={part1} exercise={exercise1}/>
-    	<Content lesson={part2} exercise={exercise2}/>
-			<Content lesson={part3} exercise={exercise3}/>
-			<Total hours={exercise1 + exercise2 + exercise3}/>
-		</div>
-  )
-}
+      <Content
+        lesson={[part1, part2, part3]}
+        exercise={[exercise1, exercise2, exercise3]}
+      />
+      <Total hours={exercise1 + exercise2 + exercise3} />
+    </div>
+  );
+};
 
 export default App;
