622c3153655ddcb9038212a1e3e060ffb6544717
lone-ion <stilos@gmail.com>
2023-11-11 21:04:39 +0200

    finish ex-2.17*: Phonebook step12

--- part2-exercise-Phonebook/src/App.jsx
+++ part2-exercise-Phonebook/src/App.jsx
@@ -5,11 +5,23 @@ import Form from './components/Form';
 import personService from './services/persons';
 import './index.css';
 
+const Notification = ({ message1, message2 }) => {
+  if (message1 === null && message2 === null) {
+    return null;
+  } else if (message1) {
+    return <div className='info'>{message1}</div>;
+  } else {
+    return <div className='error'>{message2}</div>;
+  }
+};
+
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
+  const [infoMessage, setInfoMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     personService.getAll().then((initialPersons) => {
@@ -28,6 +40,10 @@ const App = () => {
     const exists = names.indexOf(newName);
     if (exists === -1) {
       personService.create(personObject).then((returnedPerson) => {
+        setInfoMessage(`Added ${returnedPerson.name}`);
+        setTimeout(() => {
+          setInfoMessage(null);
+        }, 5000);
         setPersons(persons.concat(returnedPerson));
         setNewName('');
         setNewNumber('');
@@ -37,19 +53,31 @@ const App = () => {
         window.confirm(
           `${newName} is already added to phonebook,replace the old number with a new one?`
         )
-      );
-      const matchedNameaArrIndex = exists;
-      const objId = persons[matchedNameaArrIndex].id;
-      const changedPersonObject = { ...personObject, number: newNumber };
-      personService
-        .update(objId, changedPersonObject)
-        .then((returnedPerson) => {
-          setPersons(
-            persons.filter((obj) => obj.id !== objId).concat(returnedPerson)
-          );
-          setNewName('');
-          setNewNumber('');
-        });
+      ) {
+        const matchedNameaArrIndex = exists;
+        const objId = persons[matchedNameaArrIndex].id;
+        const changedPersonObject = { ...personObject, number: newNumber };
+        personService
+          .update(objId, changedPersonObject)
+          .then((returnedPerson) => {
+            setPersons(
+              persons.filter((obj) => obj.id !== objId).concat(returnedPerson)
+            );
+            setNewName('');
+            setNewNumber('');
+          })
+          .catch((error) => {
+            setErrorMessage(
+              `Information of ${changedPersonObject.name} was already removed from server`
+            );
+            setTimeout(() => {
+              setErrorMessage(null);
+            }, 5000);
+            setPersons(persons.filter((m) => m.id != objId));
+            setNewName('');
+            setNewNumber('');
+          });
+      }
     }
   };
 
@@ -75,6 +103,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message1={infoMessage} message2={errorMessage} />
       <Filter filter={filter} handleFilterChange={handleFilterChange} />
       <h2>add a new</h2>
       <Form
