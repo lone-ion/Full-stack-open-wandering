aadff4daa1b3279b786fa8e5e2e75a5c8691adcc
lone-ion <stilos@gmail.com>
2023-11-17 22:57:38 +0200

    uibuild

--- part2-notes-frontend-part2-1/src/services/notes.js
+++ part2-notes-frontend-part2-1/src/services/notes.js
@@ -16,6 +16,7 @@ const create = newObject => {
    return request.then(response => response.data)
 }
 
+
 const update = (id, newObject) => {
    const request = axios.put(`${baseUrl}/${id}`, newObject)
    return request.then(response => response.data)
