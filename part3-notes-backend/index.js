b11c7e2b0a3eb6d657aae1d090da3835c05ef6c1
lone-ion <stilos@gmail.com>
2023-12-08 23:38:36 +0200

    finish part3 - Other operations

--- part3-notes-backend/index.js
+++ part3-notes-backend/index.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const cors = require('cors')
+// const cors = require('cors')
 const Note = require('./models/note')
+const note = require('./models/note')
 
-app.use(cors())
+// app.use(cors())
 
 app.use(express.static('dist'))
 
@@ -30,48 +31,58 @@ app.get('/api/notes', (request, response) => {
 	})
 })
 
-app.get('/api/notes/:id', (request, response) => {
-	const id = Number(request.params.id)
-	const note = notes.find(note => note.id === id)
-	if (note) {
-		response.json(note)
-	} else {
-		response.status(404).end()
-	}
+app.get('/api/notes/:id', (request, response, next) => {
+	Note.findById(request.params.id)
+		.then(note => {
+			if (note) {
+				response.json(note)
+			} else {
+				response.status(404).end()
+			}
+		})
+		.catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-	const id = Number(request.params.id)
-	notes = notes.filter(note => note.id !== id)
-
-	response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+	Note.findByIdAndDelete(request.params.id)
+		.then(result => {
+			response.status(204).end()
+		})
+		.catch(error => next(error))
 })
-
-const generateId = () => {
-	const maxId = notes.length > 0
-		? Math.max(...notes.map(n => n.id))
-		: 0
-	return maxId + 1
-}
 
 app.post('/api/notes', (request, response) => {
 	const body = request.body
 
-	if (!body.content) {
+	if (body.content === undefined) {
 		return response.status(400).json({
 			error: 'content missing'
 		})
 	}
 
-	const note = {
+	const note = new Note ({
 		content: body.content,
 		important: body.important || false,
-		id: generateId(),
+	})
+
+	note.save().then(savedNote => {
+		response.json(note)
+	})
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+	const body = request.body
+
+	const note = {
+		content: body.content,
+		important: body.important,
 	}
 
-	notes = notes.concat(note)
-
-	response.json(note)
+	Note.findByIdAndUpdate(request.params.id, note, { new: true })
+		.then(updatedNote => {
+			response.json(updatedNote)
+		})
+		.catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -79,6 +90,19 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message)
+
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' })
+	}
+
+	next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
