8aa9c7f6ef38e8d7b51ebc1cab7f004fc1002365
lone-ion <stilos@gmail.com>
2023-12-18 21:09:03 +0200

    finish ex-4.4: helper functions and unit tests, step2

--- part4-exercise-BlogList/utils/list_helper.js
+++ part4-exercise-BlogList/utils/list_helper.js
@@ -2,6 +2,21 @@ const dummy = (blogs) => {
   return 1
 }
 
-module.exports = {
-  dummy
+const totalLikes = (blogs) => {
+
+  const array = blogs.map(item => item.likes)
+
+  console.table(array);
+
+  const aggregator = (sum, item) => {
+    return sum + item
+  }
+
+  return array.reduce(aggregator, 0) 
 }
+
+module.exports = {
+  dummy,
+  totalLikes
+}
+
