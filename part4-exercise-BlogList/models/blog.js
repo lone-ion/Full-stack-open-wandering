f51f00aa192a773145e722bf2946eba1a2e83795
lone-ion <stilos@gmail.com>
2024-02-27 22:37:24 +0200

    finish ex4.15: Blog List Expansion, step 3

--- part4-exercise-BlogList/models/blog.js
+++ part4-exercise-BlogList/models/blog.js
@@ -4,11 +4,15 @@ const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
-  likes: Number
+  likes: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 blogSchema.set('toJSON', {
-   getters: true 
+  getters: true
 })
 
 module.exports = mongoose.model('Blog', blogSchema)
