3743d121c4164a8e9cca1e3f040fdcaab8092882
lone-ion <stilos@gmail.com>
2024-03-06 13:41:50 +0200

    finish ex-4.16*: Blog List Expansion, step 4

--- part4-exercise-BlogList/controllers/users.js
+++ part4-exercise-BlogList/controllers/users.js
@@ -5,6 +5,10 @@ const User = require('../models/user')
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
+  if ( password.length < 4 ) {
+    return response.status(400).json({ error: 'password too short' })
+  }
+
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
