08b43c731228138d616dbfb877799e7bb2d7fb5c
lone-ion <stilos@gmail.com>
2024-03-09 16:54:36 +0200

    finish ex-4.19: Blog List Expansion, step 7

--- part4-exercise-BlogList/controllers/blogs.js
+++ part4-exercise-BlogList/controllers/blogs.js
@@ -1,19 +1,35 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
-  .find({}).populate('user', { username: 1, name: 1 })
+    .find({}).populate('user', { username: 1, name: 1 })
 
   response.json(blogs)
 })
 
+const getTokenfrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
-  const users = await User.find({})
-  const user = users[0]
+  // const users = await User.find({})
+  // const user = users[0]
+
+  const decodedToken = jwt.verify(getTokenfrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
