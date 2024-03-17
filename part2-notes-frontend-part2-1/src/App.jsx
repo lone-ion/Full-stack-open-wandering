8c5e1bf1b24d6e222350df5e8992959585cfa68a
lone-ion <stilos@gmail.com>
2024-03-17 21:59:57 +0200

    finish part5a - Saving the token to the browser's local storage

--- part2-notes-frontend-part2-1/src/App.jsx
+++ part2-notes-frontend-part2-1/src/App.jsx
@@ -41,6 +41,15 @@ const App = () => {
 
   useEffect(hook, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedNoteappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      noteService.setToken(user.token)
+    }
+  }, [])
+
   const addNote = (event) => {
     event.preventDefault()
     const noteObject = {
@@ -87,6 +96,8 @@ const App = () => {
         username,
         password,
       })
+
+      window.localStorage.setItem('loggedNoteappUser', JSON.stringify(user))
 
       noteService.setToken(user.token)
       setUser(user)
