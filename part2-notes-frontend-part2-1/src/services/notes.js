64bc44a09475b34534e6004cd8cd3d88a6464c5e
lone-ion <stilos@gmail.com>
2024-03-25 12:25:46 +0200

    finish part5b - ESlint

--- part2-notes-frontend-part2-1/src/services/notes.js
+++ part2-notes-frontend-part2-1/src/services/notes.js
@@ -3,28 +3,28 @@ const baseUrl = '/api/notes'
 
 let token = null
 
-const setToken = newToken => {
-   token = `Bearer ${newToken}`
+const setToken = (newToken) => {
+  token = `Bearer ${newToken}`
 }
 
 const getAll = () => {
-   const request = axios.get(baseUrl)
-   return request.then(response => response.data)
+  const request = axios.get(baseUrl)
+  return request.then((response) => response.data)
 }
 
-const create = async newObject => {
-   const config = {
-      headers: { Authorization: token },
-   }
+const create = async (newObject) => {
+  const config = {
+    headers: { Authorization: token },
+  }
 
-   const response = await axios.post(baseUrl, newObject, config)
-   return response.data
+  const response = await axios.post(baseUrl, newObject, config)
+  return response.data
 }
 
 const update = (id, newObject) => {
-   console.log(`${baseUrl} /${id}`);
-   const request = axios.put(`${baseUrl}/${id}`, newObject)
-   return request.then(response => response.data)
+  console.log(`${baseUrl} /${id}`)
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then((response) => response.data)
 }
 
 export default { getAll, create, update, setToken }
